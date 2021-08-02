a340be250cf2e62c597bb5668f858bcd3756219c
German <fuchsoria@protonmail.ch>
2021-08-02 17:32:15 +0200

    Update tests

--- tests/go.mod
+++ tests/go.mod
@@ -3,8 +3,10 @@ module github.com/Fuchsoria/banners-rotation/tests
 go 1.16
 
 require (
+	github.com/Fuchsoria/banners-rotation v0.0.0-20210801180016-5a1baa2758ee
 	github.com/google/uuid v1.3.0
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/lib/pq v1.10.2
+	github.com/streadway/amqp v1.0.0
 	github.com/stretchr/testify v1.7.0
 )
--- tests/http_test.go
+++ tests/http_test.go
@@ -8,9 +8,12 @@ import (
 	"os"
 	"testing"
 
+	simpleproducer "github.com/Fuchsoria/banners-rotation/internal/amqp/producer"
+	sqlstorage "github.com/Fuchsoria/banners-rotation/internal/storage/sql"
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"github.com/streadway/amqp"
 	"github.com/stretchr/testify/require"
 )
 
@@ -65,9 +68,17 @@ type ClickDB struct {
 	Date         string `db:"date"`
 }
 
+type ViewDB struct {
+	BannerID     string `db:"banner_id"`
+	SlotID       string `db:"slot_id"`
+	SocialDemoID string `db:"social_demo_id"`
+	Date         string `db:"date"`
+}
+
 var (
 	HTTPHost    = os.Getenv("TESTS_HTTP_HOST")
 	PostgresDSN = os.Getenv("TESTS_POSTGRES_DSN")
+	AmpqDSN     = os.Getenv("TESTS_AMQP_DSN")
 )
 
 func init() {
@@ -76,8 +87,139 @@ func init() {
 	}
 
 	if PostgresDSN == "" {
+		PostgresDSN = "amqp://guest:guest@rabbit_test:5672/"
+	}
+
+	if AmpqDSN == "" {
 		PostgresDSN = "host=0.0.0.0 port=5432 user=postgres password=example dbname=banners-rotation_test sslmode=disable"
 	}
+}
+
+func TestStorage(t *testing.T) {
+	conn, err := amqp.Dial(AmpqDSN)
+	require.NoError(t, err)
+
+	db, err := sqlx.ConnectContext(context.Background(), "postgres", PostgresDSN)
+	require.NoError(t, err)
+
+	producer := simpleproducer.New("banners-rotation", conn)
+	err = producer.Connect()
+	require.NoError(t, err)
+
+	storage, err := sqlstorage.New(context.Background(), PostgresDSN, producer)
+	require.NoError(t, err)
+
+	t.Run("test banner create", func(t *testing.T) {
+		id := uuid.NewString()
+
+		_, err := storage.CreateBanner(id, "")
+		require.NoError(t, err)
+
+		var banner ItemDB
+
+		err = db.Get(&banner, "SELECT * FROM banners WHERE id=$1", id)
+		require.NoError(t, err, "should be without errors")
+		require.Equal(t, id, banner.ID, "item should be created in db")
+	})
+
+	t.Run("test slot create", func(t *testing.T) {
+		id := uuid.NewString()
+
+		_, err := storage.CreateSlot(id, "")
+		require.NoError(t, err)
+
+		var slot ItemDB
+
+		err = db.Get(&slot, "SELECT * FROM slots WHERE id=$1", id)
+		require.NoError(t, err, "should be without errors")
+		require.Equal(t, id, slot.ID, "item should be created in db")
+	})
+
+	t.Run("test social demo create", func(t *testing.T) {
+		id := uuid.NewString()
+
+		_, err := storage.CreateSocialDemo(id, "")
+		require.NoError(t, err)
+
+		var socialDemo ItemDB
+
+		err = db.Get(&socialDemo, "SELECT * FROM social_demos WHERE id=$1", id)
+		require.NoError(t, err, "should be without errors")
+		require.Equal(t, id, socialDemo.ID, "item should be created in db")
+	})
+
+	t.Run("test add banner to rotation", func(t *testing.T) {
+		bannerID := uuid.NewString()
+		slotID := uuid.NewString()
+
+		err := storage.AddBannerRotation(bannerID, slotID)
+		require.NoError(t, err)
+
+		var rotation RotationDB
+
+		err = db.Get(&rotation, "SELECT * FROM banners_rotation WHERE banner_id=$1 AND slot_id=$2", bannerID, slotID)
+		require.NoError(t, err, "should be without errors")
+		require.Equal(t, bannerID, rotation.BannerID, "item should be created in db")
+		require.Equal(t, slotID, rotation.SlotID, "item should be created in db")
+	})
+
+	t.Run("test remove banner from rotation", func(t *testing.T) {
+		bannerID := uuid.NewString()
+		slotID := uuid.NewString()
+
+		_, err := db.Query("INSERT INTO banners_rotation (slot_id, banner_id) VALUES ($1, $2)", slotID, bannerID)
+		require.NoError(t, err)
+
+		err = storage.RemoveBannerRotation(bannerID, slotID)
+		require.NoError(t, err)
+
+		var rotation []RotationDB
+
+		err = db.Select(&rotation, "SELECT * FROM banners_rotation WHERE banner_id=$1 AND slot_id=$2", bannerID, slotID)
+		require.NoError(t, err, "should be without errors")
+		require.Len(t, rotation, 0, "selected rotation should be empty")
+	})
+
+	t.Run("test add banner click", func(t *testing.T) {
+		bannerID := uuid.NewString()
+		slotID := uuid.NewString()
+		socialDemoID := uuid.NewString()
+
+		err := storage.AddClickEvent(bannerID, slotID, socialDemoID)
+		require.NoError(t, err)
+
+		var click ClickDB
+
+		err = db.Get(&click, "SELECT * FROM clicks WHERE slot_id=$1 AND banner_id=$2 AND social_demo_id=$3", slotID, bannerID, socialDemoID)
+		require.NoError(t, err, "should be without errors")
+		require.Equal(t, bannerID, click.BannerID, "item should be created in db")
+		require.Equal(t, slotID, click.SlotID, "item should be created in db")
+		require.Equal(t, socialDemoID, click.SocialDemoID, "item should be created in db")
+		require.NotEmpty(t, click.Date, "date should exist")
+	})
+
+	t.Run("test add banner views", func(t *testing.T) {
+		bannerID := uuid.NewString()
+		slotID := uuid.NewString()
+		socialDemoID := uuid.NewString()
+
+		err := storage.AddViewEvent(bannerID, slotID, socialDemoID)
+		require.NoError(t, err)
+
+		var view ViewDB
+
+		err = db.Get(&view, "SELECT * FROM views WHERE slot_id=$1 AND banner_id=$2 AND social_demo_id=$3", slotID, bannerID, socialDemoID)
+		require.NoError(t, err, "should be without errors")
+		require.Equal(t, bannerID, view.BannerID, "item should be created in db")
+		require.Equal(t, slotID, view.SlotID, "item should be created in db")
+		require.Equal(t, socialDemoID, view.SocialDemoID, "item should be created in db")
+		require.NotEmpty(t, view.Date, "date should exist")
+	})
+
+	// storage.GetBannersClicks()
+	// storage.GetBannersViews()
+	// storage.GetNotViewedBanners()
+	// storage.GetBannersInSlot()
 }
 
 func TestHTTP(t *testing.T) {
